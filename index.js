de57106467c751cb0d3ebdb7a3d7cfd3e5627956
Ada He <ada.he0908@gmail.com>
2026-02-02 03:17:13 -0800

    Added hidden form field for handling put and delete requests, reverting to deafault form submission behaviors for x-www-formurlencoded data. Added a results section to display php echo from processing json formatted requests, since fetch won't allow for window changing and php data will not be echoed back on the php page when it is grabbed by fetch.

--- index.js
+++ index.js
@@ -18,23 +18,40 @@ function sendRequest(event){
     const action = endpoints[selectedLang];
     if (!action) throw new Error('Invalid language selected! Please stick to dropdown options.');
 
-    let url = action;
-    let options = { method: selectedMethod };
-    
-    if (selectedMethod === 'GET') {
-        url += '?' + new URLSearchParams(data);
+    if (selectedMethod === 'PUT' || selectedMethod === 'DELETE') {
+        document.getElementById('override').value = selectedMethod;
+        form.method = 'POST';
     } else {
-        if (encodeAsJSON) {
-            options.headers = { 'Content-Type':'application/json' };
-            options.body = JSON.stringify(data);
-        } else {
-            options.headers = { 'Content-Type':'application/x-www-form-urlencoded' };
-            options.body = new URLSearchParams(data);
-        }
+        form.method = selectedMethod;
     }
-
-    fetch(url, options)
-        .then(res => res.text());
+    form.action = action;
+    
+    if (encodeAsJSON) {
+        // Include override field in JSON for PUT/DELETE
+        if (selectedMethod === 'PUT' || selectedMethod === 'DELETE') {
+            data._method = selectedMethod;
+        }
+        
+        fetch(form.action, {
+            method: (selectedMethod === 'PUT' || selectedMethod === 'DELETE') ? 'POST' : selectedMethod,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: (selectedMethod === 'GET') ? undefined : JSON.stringify(data)
+        })
+        .then(response => response.text())
+        .then(data => {
+            // Display or process the JSON response
+            console.log('Response:', data);
+            document.getElementById('results').innerHTML += 
+                `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    } else {
+        form.submit();
+    }
 }
 
 function getFormData(form){
