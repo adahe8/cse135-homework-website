653bc7d8606e37a96b8e3c5d3b921adeefdb2676
Ada He <ada.he0908@gmail.com>
2026-02-01 23:57:34 -0800

    Added handling for whether data is to be sent in json form or in html url encoded form based on form inputs.

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const form = document.getElementById('echo-form');
 const langType = document.getElementById('scripting-lang');
 const httpMethod = document.getElementById('http-method');
 
-form.addEventListener('submit', sendRequest)
+form.addEventListener('submit', sendRequest);
 
 function sendRequest(event){
     event.preventDefault();
-
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    const encodeAsJSON = document.getElementById('json').checked;
     let selectedLang = langType.value;
     let selectedMethod = httpMethod.value;
+
 
     if (selectedLang == 'PHP'){
         this.action = "../../usr/lib/cgi-bin/echo-php.php";
@@ -16,8 +19,39 @@ function sendRequest(event){
         this.action = "../../usr/lib/cgi-bin/echo-ruby.rb";
     } else if (selectedLang == 'Node.js'){
         this.action = "../../usr/lib/cgi-bin/echo-node-express.js";
+    }else{
+        throw new Error("Pick a language from the ones available only!");
     }
     this.method = selectedMethod;
     
-    this.submit();
+    if (encodeAsJSON) {
+        if (httpMethod == 'POST' || httpMethod == 'PUT'){
+            fetch(this.action, {
+                method: selectedMethod,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+        } else if (httpMethod == 'GET' || httpMethod == 'DELETE'){
+            fetch(this.action, {
+                method: selectedMethod,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(response => response.json())
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+        }
+        
+    } else {
+        this.submit();
+    }
+
 }
