935e956d9fad09ffe80c105aaadd0c83f4b7f827
CarmenHe <43501759+CarmenHe@users.noreply.github.com>
2026-02-02 22:57:41 -0800

    experimenting with analytics tracking on ruby generated pages

--- ruby-copy/hello-json-ruby.rb
+++ ruby-copy/hello-json-ruby.rb
@@ -1,20 +1,15 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 require 'json'
-# require 'cgi'
 
 # HTTP header - not necessary if using cgi library, which does request handling
-puts "Cache-Control: no-cache";
-puts "Content-Type: application/json\n";
+print "Cache-Control: no-cache\n";
+print "Content-Type: application/json\n\n";
 
 # Store data to include in response
 date = Time.now;
 address = ENV['REMOTE_ADDR'] || "Unknown";
 
-message = {message: 'Created with ♥ by Carmen & Ada He with Ruby CGI!', date: date, currentIP: address};
-
-# if using CGI library
-# cgi = CGI.new
-# puts cgi.header("type" => "application/json")
+message = {:message => 'Created with ♥ by Carmen & Ada He with Ruby CGI!', :date => date, :currentIP => address};
 
 # output JSON
 puts JSON.generate(message);
